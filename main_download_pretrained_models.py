9c65dd59c8f8bec7f8b3c0672154b8ea0436b54c
andysuh13 <andysuh13@naver.com>
2024-03-16 16:42:37 +0900

    테스트테스트

--- main_download_pretrained_models.py
+++ main_download_pretrained_models.py
@@ -13,6 +13,7 @@ download models:
 """
 
 # 추가해보기
+# fetch를 해보기 위한 테스트
 
 def download_pretrained_model(model_dir='model_zoo', model_name='dncnn3.pth'):
     if os.path.exists(os.path.join(model_dir, model_name)):
